af489ef3007c7b2f9c690f8e4d6fb587e0d8b7ff
Bridgette Salcido <bls.salcido@gmail.com>
2014-05-11 17:40:12 -1000

    adding files

--- week_6/1_assert_statements/my_solution.rb
+++ week_6/1_assert_statements/my_solution.rb
@@ -1,6 +1,6 @@
 # U2.W6: Testing Assert Statements
 
-# I worked on this challenge [by myself].
+# I worked on this challenge [by myself -- see line 72 for partner solution].
 
 
 # 2. Review the simple assert statement
@@ -67,3 +67,66 @@ assert {card_2.check_card == false}
 # initially I had assert {card_1 == false} and it just didn't look right. I changed it and it 
 # worked. I'm very confident about this exercise so, I'll probably do the rspec challenge if 
 # I have time. 
+
+
+##Did this exercise a second time with Danny Glover. 
+
+
+class CreditCard
+  def initialize(credit_card_number) 
+    raise ArgumentError.new("Invalid Credit Card Number.") if credit_card_number.to_s.length != 16
+    @credit_card_number = credit_card_number.to_s.split("")
+  end
+  
+
+  def check_card
+    verifiable_card_num = []
+  
+    @credit_card_number.each_with_index { |item,index| 
+      if index % 2 != 0
+        verifiable_card_num << item.to_i
+      elsif index % 2 == 0 
+        verifiable_card_num << (item.to_i * 2) 
+      end }
+ 
+    check_card_num = verifiable_card_num.to_s.split("")
+  
+    check_card_num.map!{|x| x.to_i}.inject(:+)
+ 
+    return true if check_card_num.reduce(:+) % 10 == 0 
+    return false if check_card_num.reduce(:+) % 10 !=0
+  end  
+end
+
+
+# 4. Convert your driver test code from that challenge into Assert Statements
+
+# puts card_1.check_card == true 
+# puts card_2.check_card == true
+# puts card_3.check_card == true 
+# puts card_4.check_card == true
+# puts card_5.check_card == true 
+
+card_1 = CreditCard.new(4563960122001999) 
+card_2 = CreditCard.new(4532679350038774)
+card_3 = CreditCard.new(4532642548902343)
+card_4 = CreditCard.new(4716142602810621)
+card_5 = CreditCard.new(4485720303618200)
+
+
+def check_card_assertion
+  raise "Credit Card Not Valid!" unless yield 
+end  
+
+check_card_assertion {card_1.check_card}
+check_card_assertion {card_2.check_card}
+check_card_assertion {card_3.check_card}
+check_card_assertion {card_4.check_card}
+check_card_assertion {card_5.check_card}
+
+## Reflection 
+
+# It was pretty fun working on this with Danny becasue we figuerd out that 
+# assert still works no matter what you name it. We were undecided what the 
+# proper convention is - whether it's best to avoid doing this or not - 
+# but, it works and that was an interesting find. 
--- week_6/5_refactoring/my_solution.rb
+++ week_6/5_refactoring/my_solution.rb
@@ -1,99 +1,120 @@
 # U2.W6: Refactoring for Code Readability
 
 
-# I worked on this challenge [by myself].
+# I worked on this challenge [with Rj Bernaldo].
+
+
+# I worked on this challenge with:
+# 1) Bridgette Salcido
+# 2) RJ Bernaldo
 
 
 # Original Solution
-
-class CreditCard
-    def initialize (credit_card_number)
-        @credit_card_number = credit_card_number  
-        @credit_card_number = @credit_card_number.to_s
-        @credit_card_number.split(//)   
-        if @credit_card_number != 16
-        raise ArgumentError.new("The credit card number must be 16 digits") 
-        else
-
-        
+class CreditCard_OLD
+  def initialize(credit_card_number) 
+    raise ArgumentError.new("Invalid Credit Card Number.") if credit_card_number.to_s.length != 16
+    @credit_card_number = credit_card_number.to_s.split("")
+  end
+  
+  def check_card
+    verifiable_card_num = []
+  
+    @credit_card_number.each_with_index { |item,index| 
+      if index % 2 != 0 # is odd?         
+        verifiable_card_num << item.to_i
+      elsif index % 2 == 0 # is even? 
+        verifiable_card_num << (item.to_i * 2) 
+      end }
  
-        @unmodified = []
-        @modfied = []
-        @modified_final = []
-
-      end 
-      end
-    
-
-    def check_card
-        @credit_card_number.each_with_index do |item, index| 
-           if index % 2 == 0
-           item = item * 2
-           @modfied.push(item)
-           else
-           @unmodified.push(item)
-           end
-
-         
-           
-
-       @modified_final = @modified.join('').split(//)
-       @modified_final.map { |string_val| string_val.to_i }
-    	 @total = (@unmodified.concat(@modified_final).inject(:+)
-
-   
-            
-        end
-       end
-     end
-   end
-
+    check_card_num = verifiable_card_num.to_s.split("")
+  
+    check_card_num.map!{|x| x.to_i}
+ 
+    return true if check_card_num.reduce(:+) % 10 == 0 
+    return false if check_card_num.reduce(:+) % 10 !=0
+  end  
+end
 
 # Refactored Solution
 
 class CreditCard
-  def initialize (credit_card_number)
-    @credit_card_number = credit_card_number  
-    @credit_card_number = @credit_card_number.to_s
-    @credit_card_number.split(//)   
-    if @credit_card_number != 16
-      raise ArgumentError.new("The credit card number must be 16 digits") 
-    else
-
-    @unmodified = []
-    @modfied = []
-    @modified_final = []
-
-    end 
+  
+  def initialize(credit_card_number)
+    raise ArgumentError.new("Invalid Credit Card Number.") if credit_card_number.to_s.length != 16
+    @credit_card_number = credit_card_number.to_s.split(//)
   end
-    
-
+  
   def check_card
-    @credit_card_number.each_with_index do |item, index| 
-    if index % 2 == 0
-      item = item * 2
-      @modfied.push(item)
-    else
-      @unmodified.push(item)
-    end
-
-    @modified_final = @modified.join('').split(//)
-    @modified_final.map { |string_val| string_val.to_i }
-    @total = (@unmodified.concat(@modified_final).inject(:+)
-
-   end
+    
+    check_card_num = @credit_card_number.inject([]) { |verifiable_card_num, item|
+      if @credit_card_number.index(item).odd?
+        verifiable_card_num << item.to_i
+      elsif @credit_card_number.index(item).even?
+        verifiable_card_num << (item.to_i * 2)
+      end
+      ; verifiable_card_num
+    }#.to_s.split("").map!{|x| x.to_i}
+    
+    #check_card_num = check_card_num.join("").split("").map {|x| x.to_i}
+    #check_card_num = check_card_num.map {|x| x.to_i}
+    
+    #check_card_num = verifiable_card_num.to_s.split("")
+    
+    #check_card_num.map!{|x| x.to_i}
+ 
+    # if check_card_num.reduce(:+) % 10 == 0 
+    #   true
+    # else
+      check_card_num#.inject(:+) % 10
+    #end
   end
- end
 end
 
-
-
+#coordinates.inject([]) { |word, coordinate| word << coordinate ; word }
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+card_1 = CreditCard.new(4563960122001999) 
+# card_2 = CreditCard.new(4532679350038774)
+# card_3 = CreditCard.new(4532642548902343)
+# card_4 = CreditCard.new(4716142602810621)
+# card_5 = CreditCard.new(4485720303618200)
 
 
+p card_1.check_card# == true 
+# p card_2.check_card# == true
+# p card_3.check_card# == true 
+# p card_4.check_card# == true
+# p card_5.check_card# == true 
+
+# def assert
+#   raise "Credit Card Not Valid!" unless yield 
+# end  
+
+
+# assert {card_1.check_card}
+# assert {card_2.check_card}
+# assert {card_3.check_card}
+# assert {card_4.check_card}
+# assert {card_5.check_card}
 
 
 
 # Reflection 
+
+# Found only a few things to refactor further so, decided to change our strategy 
+# altogether instead. 
+# We really wanted to try using #inject to invoke a block of code because neither 
+# Rj or myself have had much experience with this. So...then we broke it....a lot. 
+# I feel satisfied by this challenge because we learned so much along the way. We're 
+# still trying and have already scheduled a pair session for Monday, May 13th to 
+# continue working on this together. I think we got pretty close, it was tripping us 
+# up that we had an array of [odd, odd, odd, even*2, even*2, even*2] for a while 
+# before we realized that we had been appending an empty array with numbers out of 
+# order as compared with how we need them to be organized in the array to proceed 
+# in our method. So, we finally fixed that after lots of testing, which was really 
+# fun. I really want to figure this out and think we can with a little more time. 
+# To be continued! 
+
+
+
